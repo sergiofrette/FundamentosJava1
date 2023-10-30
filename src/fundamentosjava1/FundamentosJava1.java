e352235f2126153396f59fc5fa89eb23d315045d
frette sergio <frettesergio@gmail.com>
2023-10-30 12:46:59 -0300

    condicionales

--- src/fundamentosjava1/FundamentosJava1.java
+++ src/fundamentosjava1/FundamentosJava1.java
@@ -15,7 +15,36 @@ public class FundamentosJava1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear 3 variables una para la edad, el nombre y el salario
+        System.out.println("VARIABLES");
+        int edad;
+        String nombre = "Nico";
+        double salario = 200;
+        
+        edad = 38;
+        //mostrar por pantalla Soy <nombre> tengo <edad> y mi sueldo es de <salario>
+        System.out.println(" Soy " + nombre + " tengo " + edad + " años y mi sueldo es de " + salario);
+       
+        //seguidos
+        System.out.println("nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Salario: " + salario);
+        
+        System.out.println("----------------------------");
+        
+        System.out.println(" ESTRUCTURA DE CONTROL - CONDICIONALES");
+        
+        edad = 38;
+        if (edad >=18) {
+            System.out.println("Es mayor de 18");
+        }
+        if(edad >=38){
+            System.out.println("No esta en el rango de edad");
+        }
+        else {
+            System.out.println("Es menor de 18");
+        }
+        
     }
     
 }
